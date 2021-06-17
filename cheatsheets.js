4bb0da1d1d6bfc751cfbd12fdfc00196b75d3fb2
Lougerb <64685319+Lougerb@users.noreply.github.com>
2021-06-17 17:09:44 +0800

    Update cheatsheets.js

--- cheatsheets.js
+++ cheatsheets.js
@@ -1,4 +1,6 @@
+//=====================================================================================
 //toggle two or more element's class to be remove or added
+//=====================================================================================
 const toggleClassOnce  =  (
   elementAdd,
   addMClass,
@@ -14,3 +16,64 @@ const toggleClassOnce  =  (
     elementRemove.classList.remove(removeMClass);
   }
 };
+
+//=====================================================================================
+//Swipe functions
+//=====================================================================================
+myElement.addEventListener("touchstart", startTouch, false);
+myElement.addEventListener("touchmove", moveTouch, false);
+ 
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+ 
+function startTouch(e) {
+  initialX = e.touches[0].clientX;
+  initialY = e.touches[0].clientY;
+};
+ 
+function moveTouch(e) {
+  if (initialX === null) {
+    return;
+  }
+ 
+  if (initialY === null) {
+    return;
+  }
+ 
+  var currentX = e.touches[0].clientX;
+  var currentY = e.touches[0].clientY;
+ 
+  var diffX = initialX - currentX;
+  var diffY = initialY - currentY;
+ 
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    // sliding horizontally
+    if (diffX > 0) {
+      // swiped left
+      console.log("swiped left");
+    } else {
+      // swiped right
+      console.log("swiped right");
+    }  
+  } else {
+    // sliding vertically
+    if (diffY > 0) {
+      // swiped up
+      console.log("swiped up");
+    } else {
+      // swiped down
+      console.log("swiped down");
+    }  
+  }
+ 
+  initialX = null;
+  initialY = null;
+   
+  e.preventDefault();
+};
+
+
+//=====================================================================================
+//Title
+//=====================================================================================
